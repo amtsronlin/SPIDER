3d04cee84d776c3e6308ad6fc7f4a7324fa75127
amtsronlin <nhamchithanh@gmail.com>
2018-05-07 14:57:49 +0700

    Create home-layout.component.ts

---
+++ src/layouts/home-layout/home-layout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+@Component({
+  selector: 'app-home-layout',
+  templateUrl: `./home-layout.component.html`,
+  styleUrls: ['./home-layout.component.scss']
+})
+
+export class HomeLayoutComponent { }
